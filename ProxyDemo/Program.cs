a6f88b7ba58f6f68a18ec3af14599a6870790c5e
Ayush.Dodake <Ayush.Dodake@DELL1558>
2024-08-08 22:28:00 +0530

    Add project files.

---
+++ ProxyDemo/Program.cs
@@ -0,0 +1,12 @@
+﻿using ProxyDemo;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        var secureNestProxy = new SecureNestProxy(new RealNest());
+
+        secureNestProxy.Access("Stegosaurus");
+        secureNestProxy.Access("TRex");
+    }
+}
---
+++ ProxyDemo/SecureNestProxy.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProxyDemo
+{
+    internal class SecureNestProxy : INest
+    {
+        private readonly INest nest;
+
+        public SecureNestProxy(INest nest)
+        {
+            this.nest = nest;
+        }
+
+        public void Access(string name)
+        {
+            if (name == "TRex" || name == "Gigantosaurus")
+            {
+                throw new UnauthorizedAccessException($"{name} is not allowed to access the nest.");
+            }
+            else
+            {
+                this.nest.Access(name);
+            }
+        }
+    }
+}
+
